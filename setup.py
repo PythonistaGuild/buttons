661870ee5e378dd29fe873308a0b0dd56a09843c
Myst(MysterialPy) <mysterialpy@gmail.com>
2019-04-15 14:58:55 +1000

    bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-version = '0.1.0'
+version = '0.1.4'
 
 readme = ''
 with open('README.rst') as f:
